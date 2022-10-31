2c186c031aa8dcd3288ce90a991cafeef94cac55
Anastasiyu <asy.safiulina@mail.ru>
2022-10-31 13:25:31 +0300

    Initial commit

---
+++ src/main/java/com/example/ruhogwartsscool/RuHogwartsScoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruhogwartsscool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuHogwartsScoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuHogwartsScoolApplication.class, args);
+    }
+
+}
